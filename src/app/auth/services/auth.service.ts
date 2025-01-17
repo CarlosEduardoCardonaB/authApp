a271654e68bf925101425bc10b309d4f504b98ec
Carlos Eduardo <caeca425@gmail.com>
2025-01-16 19:08:50 -0500

    se trabaja en pantalla login y authservice ya realizando petición post al servicio y manejando respuesta con signals

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,7 +1,9 @@
-import { inject, Injectable, signal } from '@angular/core';
+import { computed, inject, Injectable, signal } from '@angular/core';
 import { environment } from '../../../environments/environments';
 import { HttpClient } from '@angular/common/http';
-import { of } from 'rxjs';
+import { map, Observable, tap } from 'rxjs';
+import { User, AuthStatus, LoginResponse } from '../interfaces';
+
 
 @Injectable({
   providedIn: 'root'
@@ -11,16 +13,33 @@ export class AuthService {
   private readonly baseUrl: string = environment.baseUrl;
   private http = inject( HttpClient );
 
-  // private _currentUser = signal<User|null>(null);
-  // private _authStatus = signal<AuthStatus>();
+  private _currentUser = signal<User|null>( null );
+  private _authStatus = signal<AuthStatus>( AuthStatus.checking );
+
+
+  public currentUser = computed( () => this._currentUser() );
+  public authStatus = computed( () => this._authStatus() );
 
   constructor() { }
 
-  login( email: string, password: string){
+  login( email: string, password: string ): Observable<boolean>{
 
+    const url = `${ this.baseUrl }/auth/login`;
+    const body = { email: email, password: password };
+    console.log(url);
+    console.log(body);
+    return this.http.post<LoginResponse>(url, body )
+      .pipe(
+        tap( ({user, token}) => {
+          this._currentUser.set( user );
+          this._authStatus.set( AuthStatus.authenticated );
+          localStorage.setItem('token', token);
+          console.log({user, token})
+        }),
+        map( () => true )
+      )
 
-
-    return of(true);
+    //return of(true);
   }
 
 }
